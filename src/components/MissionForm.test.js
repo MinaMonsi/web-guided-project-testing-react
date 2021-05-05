601efce5b0215fb75c3fa751676b3293c9881498
Josh Jacobson <joshjacobson14@gmail.com>
2021-05-04 17:08:26 -0700

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -1,3 +1,9 @@
 import React from "react";
+import {render} from "@testing-library/react"
+import MissionForm from "./MissionForm";
 
 // BDD / TDD: write the tests first, then implement the functionality to make the tests go from red to green
+
+test("MissionForm renders correctly", () => {
+    render(<MissionForm />);
+})
