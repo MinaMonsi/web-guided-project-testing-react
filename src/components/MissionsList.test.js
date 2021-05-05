bcaedb0d2cbef1e09e93562186adeaed1671f304
Josh Jacobson <joshjacobson14@gmail.com>
2021-05-04 17:57:58 -0700

    lecture work-in-progress

--- src/components/MissionsList.test.js
+++ src/components/MissionsList.test.js
@@ -9,5 +9,10 @@ import MissionsList from "./MissionsList";
 // Sad path: rerender. props.error contains an error message to display
 
 test("MissionsList displays mission names when rerendered with new data", () => {
-    render(<MissionsList missions={[]} />);
+    const { rerender } = render(<MissionsList missions={[]} />);
+
+    let renderedMissions = screen.getAllByTestId("mission");
+
+
+
 })
