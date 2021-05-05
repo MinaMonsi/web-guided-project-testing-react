5342ec24b1749cdee76ed363aaf91bc6e69bffe9
Josh Jacobson <joshjacobson14@gmail.com>
2021-05-04 18:17:26 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,8 @@
 import React from "react";
 import { fireEvent, screen, render } from "@testing-library/react"
 import App from "./App";
+
+test("renders without blowing up the spaceships!", () => {
+    render(<App />);
+});
+
