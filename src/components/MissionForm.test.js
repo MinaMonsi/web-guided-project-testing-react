59179e88e0bdc34f50823d97f867971410c4a754
Josh Jacobson <joshjacobson14@gmail.com>
2021-05-04 17:32:24 -0700

    lecture work-in-progress

--- src/components/MissionForm.test.js
+++ src/components/MissionForm.test.js
@@ -10,7 +10,7 @@ test("MissionForm renders correctly", () => {
 
 // test the logic for when the button displays vs displaying a loading message (based off the isFetchingData prop)
 
-test("Renders a loading message when isFetchingData is true", () => {
+test("Renders a loading message when fetching data", () => {
     const { getByText } = render(<MissionForm isFetchingData={true} />);
     const message = getByText(/we are fetching data/i); // implicit assertion already here
 
@@ -21,7 +21,7 @@ test("Renders a loading message when isFetchingData is true", () => {
     expect(button).toBeNull();
 });
 
-test("Renders a button when isFetchingData is false", () => {
+test("Renders a button if not currently fetching data", () => {
     render(<MissionForm isFetchingData={false} />);
     const button = screen.getByRole("button"); // implicit assertion that 1 and only 1 button is in the DOM
 
