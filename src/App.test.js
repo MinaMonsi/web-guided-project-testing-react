7a427954a21aedbd52b8ea09624ae106f97d4b5c
Josh Jacobson <joshjacobson14@gmail.com>
2021-05-04 18:20:34 -0700

    lecture work-in-progress

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@ import React from "react";
 import { fireEvent, screen, render } from "@testing-library/react"
 import App from "./App";
 
-import { fetchMissions } from "./api/fetchMissions";
+import fetchMissions from "./api/fetchMissions";
+
+jest.mock("./api/fetchMissions");
 
 test("renders without blowing up the spaceships!", () => {
     render(<App />);
